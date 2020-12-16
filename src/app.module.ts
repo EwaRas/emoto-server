e9be809afe952a47ae187de2ece81ee280fedf23
Carlos <carlosdsv@gmail.com>
2020-12-16 11:20:03 +0100

    refactor(app module): delete unnecessary service and controller files
    delete files created at the beginning used for testing the endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MapModule } from './map/map.module';
@@ -23,7 +21,6 @@ import { CurrentTripsModule } from './current-trips/current-trips.module';
     HttpModule,
     CurrentTripsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, MapboxService, FluctuoService],
+  providers: [MapboxService, FluctuoService],
 })
 export class AppModule {}
